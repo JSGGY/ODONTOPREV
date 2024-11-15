457979c3c7056ea24fa4a577c21c463a2eb70691
Alexiskeepcoding <alexis.sotomayor@epn.edu.ec>
2024-11-15 09:34:02 -0500

    feat: 15-11-2024_adding packages

--- src/backend/routes/user.routes.ts
+++ src/backend/routes/user.routes.ts
@@ -11,56 +11,55 @@ dotenv.config();
 
 router.use(bodyParser.json());
 
-// const handleError = (error: unknown, res: Response) => {
-//     if (error instanceof Error) {
-//         res.status(400).json({ error: error.message });
-//     } else {
-//         res.status(400).json({ error: "Unknown error" });
-//     }
-// };
+const handleError = (error: unknown, res: Response) => {
+    if (error instanceof Error) {
+        res.status(400).json({ error: error.message });
+    } else {
+        res.status(400).json({ error: "Unknown error" });
+    }
+};
 
 
+router.get("/api/users", async (req: Request, res: Response) => {
+    try {
+        const users = await prisma.user.findMany();
+        res.json(users);
+    } catch (error: unknown) {
+        handleError(error, res);
+    }
+});
 
-// router.get("/api/users", async (req: Request, res: Response) => {
-//     try {
-//         const users = await prisma.user.findMany();
-//         res.json(users);
-//     } catch (error: unknown) {
-//         handleError(error, res);
-//     }
-// });
-
-// router.post("/api/user/:id", async (req: Request, res: Response) => {
-//     try {
-//         const { id } = req.params;
-//         const user = await prisma.user.create({
-//             data: {
-//                 id,
-//                 name: req.body.name,
-//                 email: req.body.email,
-//                 password: req.body.password,
-//             },
-//         });
-//         res.json(user);
-//     } catch (error: unknown) {
-//         handleError(error, res);
-//     }
-// });
+router.post("/api/user/:id", async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const user = await prisma.user.create({
+            data: {
+                id: Number(id),
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+            },
+        });
+        res.json(user);
+    } catch (error: unknown) {
+        handleError(error, res);
+    }
+});
 
 
-// router.delete("/api/user/:id", async (req: Request, res: Response) => {
-//     try {
-//         const { id } = req.params;
-//         const user = await prisma.user.delete({
-//             where: {
-//                 id,
-//             },
-//         });
-//         res.json(user);
-//     } catch (error: unknown) {
-//         handleError(error, res);
-//     }
-// });
+router.delete("/api/user/:id", async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const user = await prisma.user.delete({
+            where: {
+                id: Number(id),
+            },
+        });
+        res.json(user);
+    } catch (error: unknown) {
+        handleError(error, res);
+    }
+});
 
 
 export default router;
